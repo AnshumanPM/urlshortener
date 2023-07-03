0e913117399f641fcbcc86c3aedae355ae0b8ac5
AnshumanPM <anshumanprasadmahanta@gmail.com>
2023-07-03 04:25:57 +0000

    Auto: Code Formatted

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import sqlite3
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
 from hashids import Hashids
-from flask import Flask, jsonify, render_template, request, flash, redirect, url_for
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "for the night is dark and full of terrors"
+app.config["SECRET_KEY"] = "for the night is dark and full of terrors"
 app.config["JSON_SORT_KEYS"] = False
 
-hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+hashids = Hashids(min_length=4, salt=app.config["SECRET_KEY"])
 
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route("/", methods=("GET", "POST"))
 def index():
     conn = get_db_connection()
 
-    if request.method == 'POST':
-        url = request.form['url']
+    if request.method == "POST":
+        url = request.form["url"]
 
         if not url:
-            flash('please enter a URL!')
-            return redirect(url_for('index'))
-        
-        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)', (url,))
+            flash("please enter a URL!")
+            return redirect(url_for("index"))
+
+        url_data = conn.execute("INSERT INTO urls (original_url) VALUES (?)", (url,))
         conn.commit()
         conn.close()
 
@@ -34,22 +36,22 @@ def index():
         hashid = hashids.encode(url_id)
         short_url = request.host_url + hashid
 
-        return render_template('index.html', short_url=short_url)
-    
-    return render_template('index.html')
+        return render_template("index.html", short_url=short_url)
+
+    return render_template("index.html")
 
 
-@app.route('/api/v1')
+@app.route("/api/v1")
 def api():
     conn = get_db_connection()
 
     url = request.args.get("url")
-    
+
     if url != "":
-        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)', (url,))
+        url_data = conn.execute("INSERT INTO urls (original_url) VALUES (?)", (url,))
         conn.commit()
         conn.close()
-    
+
         url_id = url_data.lastrowid
         hashid = hashids.encode(url_id)
         short_url = request.host_url + hashid
@@ -60,47 +62,48 @@ def api():
         return jsonify(response_data)
 
 
-@app.route('/<id>')
+@app.route("/<id>")
 def url_redirect(id):
     conn = get_db_connection()
 
     original_id = hashids.decode(id)
     if original_id:
         original_id = original_id[0]
-        url_data = conn.execute('SELECT original_url, clicks FROM urls'
-                                ' WHERE id = (?)', (original_id,)
-                                ).fetchone()
-        original_url = url_data['original_url']
-        clicks = url_data['clicks']
+        url_data = conn.execute(
+            "SELECT original_url, clicks FROM urls" " WHERE id = (?)", (original_id,)
+        ).fetchone()
+        original_url = url_data["original_url"]
+        clicks = url_data["clicks"]
 
-        conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',
-                     (clicks+1, original_id))
+        conn.execute(
+            "UPDATE urls SET clicks = ? WHERE id = ?", (clicks + 1, original_id)
+        )
 
         conn.commit()
         conn.close()
         return redirect(original_url)
     else:
-        flash('Invalid URL')
-        return redirect(url_for('index'))
+        flash("Invalid URL")
+        return redirect(url_for("index"))
 
 
-@app.route('/stats')
+@app.route("/stats")
 def stats():
     conn = get_db_connection()
-    db_urls = conn.execute('SELECT id, created, original_url, clicks FROM urls'
-                           ).fetchall()
+    db_urls = conn.execute(
+        "SELECT id, created, original_url, clicks FROM urls"
+    ).fetchall()
     conn.close()
 
     urls = []
     for url in db_urls:
         url = dict(url)
         print(url)
-        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        url["short_url"] = request.host_url + hashids.encode(url["id"])
         urls.append(url)
 
-    return render_template('stats.html', urls=urls)
+    return render_template("stats.html", urls=urls)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
